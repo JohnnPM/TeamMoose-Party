e0856f9962c4754babdfea0437be2450554200e6
xXJarJarXx <598Johnn897@gmail.com>
2014-12-30 00:02:10 -0500

    Added packages, plugin.yml, pom.xml for later usage

---
+++ src/org/teammoose/party/TMParty.java
@@ -0,0 +1,17 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Dec 29, 2014
+ * Package: org.teammoose.party
+ */
+package org.teammoose.party;
+
+/**
+ * 
+ * @author 598Johnn897
+ * @since
+ */
+public class TMParty
+{
+
+}
---
+++ src/org/teammoose/party/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Author: 598Johnn897
+ * 
+ * Date: Dec 29, 2014
+ * Package: org.teammoose.party
+ */
+/**
+ * 
+ * @author 598Johnn897
+ * @since
+ */
+package org.teammoose.party;
